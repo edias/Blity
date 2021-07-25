d6bc440602d266434577dbf7a5264d086343ab61
Eduardo Dias <eduardo.dias@iag.co.nz>
2021-07-25 17:11:04 +1200

    Initial commit

---
+++ Blity/BlityApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlityApp.swift
+//  Blity
+//
+//  Created by Eduardo Dias on 25/07/21.
+//
+
+import SwiftUI
+
+@main
+struct BlityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
